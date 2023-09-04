1cfd88bec3207d6e787212ac9653a22fc2a89ed6
madnstar <dmarchant@cnachile.cl>
2023-09-04 20:42:12 -0300

    interfaz base

--- interfaz.py
+++ interfaz.py
@@ -1,1 +1,11 @@
 import tkinter as tk
+import customtkinter as ctk
+
+ctk.set_appearance_mode('system')
+ctk.set_default_color_theme('blue')
+
+app = ctk.CTk()
+app.geometry('720x480')
+app.title('MI')
+
+app.mainloop()
