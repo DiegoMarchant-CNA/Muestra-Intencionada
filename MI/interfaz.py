8c61e5670489fb6da8d785c7244e706241945a26
Felipe Olivares <felipeolivares.it@gmail.com>
2023-09-29 02:27:56 -0300

    Refactorización de interfaz en clases

--- MI/interfaz.py
+++ MI/interfaz.py
@@ -48,246 +48,259 @@ for nombre_directorio in ["", "selección/", "elegibles/"]:
                 raise
 
 # Configuración inicial de la app
-
-
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('CNA_colors')
 
-# Instanciar la app como ventana
 
-app = ctk.CTk()
-app.geometry('1366x718')
-app.title('Muestra Intencionada - CNA Chile')
-app.after(201, lambda: app.iconbitmap('icon.ico'))
-interfaz_log.info('Inicializar interfaz grafica')
+class App(ctk.CTk):
+    """Ventana principal y propiedades de pestañas."""
+    def __init__(self):
+        super().__init__()
 
-# Agregar barra inicial con tabs
+        self.geometry('1366x718')
+        self.title('Muestra Intencionada - CNA Chile')
+        self.after(201, lambda: self.iconbitmap('icon.ico'))
+        interfaz_log.info('Inicializar interfaz grafica')
 
-tabview = ctk.CTkTabview(master=app)
-tabview.pack(anchor=ctk.NW,
-             fill='both',
-             expand=True)
+        self.tabview = MyTabView(master=self)
+        self.tabview.pack(anchor=ctk.NW,
+                          fill='both',
+                          expand=True)
+        self.tabview.set("Inicio")  # set currently visible tab
 
-tabview.add("Inicio")  # add tab at the end
-tabview.add("Elegibles")  # add tab at the end
-tabview.add("Selección")  # add tab at the end
-tabview.set("Inicio")  # set currently visible tab
+        self.frame_inicio = FrameInicio(
+                master=self.tabview.tab('Inicio'),
+                width=1366,
+                height=718)
+        self.frame_inicio.pack()
 
-# ---------------------------------------------------------------------
-# Pestaña con descripción inicial
+        self.frame_elegibles = FrameElegibles(
+                master=self.tabview.tab('Elegibles'),
+                width=1366,
+                height=718)
+        self.frame_elegibles.pack(fill='both',
+                                  expand=True)
 
-frame_inicio = ctk.CTkFrame(master=tabview.tab('Inicio'),
-                            width=1366,
-                            height=718)
-
-frame_inicio.pack()
-
-# ---------------------------------------------------------------------
-# Pestaña para ejecutar programa de elegibilidad
-
-Oferta_path = ''
-Matricula_path = ''
-Titulados_path = ''
-
-
-def select_file_oferta():
-    """Función para botón de selección archivo Oferta"""
-    filetypes = (
-        ('csv (archivo separado por comas)', '.csv'),
-        ('Todos los archivos', '*.*')
-    )
-
-    global Oferta_path
-    file = fd.askopenfilename(
-        title='Abrir archivo',
-        initialdir='/',
-        filetypes=filetypes)
-    if file != '':
-        Oferta_boton.configure(text=file)
-    Oferta_path = file
+        self.frame_seleccion = FrameSeleccion(
+                master=self.tabview.tab('Selección'),
+                width=1366,
+                height=718)
+        self.frame_seleccion.pack(expand=True,
+                                  fill='both')
 
 
-def select_file_matricula():
-    """Función para botón de selección archivo Matrícula"""
-    filetypes = (
-        ('csv (archivo separado por comas)', '.csv'),
-        ('Todos los archivos', '*.*')
-    )
+class MyTabView(ctk.CTkTabview):
+    """Pestañas disponibles."""
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-    global Matricula_path
-    file = fd.askopenfilename(
-        title='Abrir archivo',
-        initialdir='/',
-        filetypes=filetypes)
-    if file != '':
-        Matricula_boton.configure(text=file)
-    Matricula_path = file
-
-
-def select_file_titulados():
-    """Función para botón de selección archivo Titulados"""
-    filetypes = (
-        ('csv (archivo separado por comas)', '.csv'),
-        ('Todos los archivos', '*.*')
-    )
-
-    global Titulados_path
-    file = fd.askopenfilename(
-        title='Abrir archivo',
-        initialdir='/',
-        filetypes=filetypes)
-    if file != '':
-        Titulados_boton.configure(text=file)
-    Titulados_path = file
-
-
-def Run_Main():
-    Main(outputfolder,
-         Oferta_path,
-         Matricula_path,
-         Titulados_path)
-    Run_Main_boton.configure(text='Elegibles listos!')
-
-
-frame_elegibles = ctk.CTkFrame(master=tabview.tab('Elegibles'),
-                               width=1366,
-                               height=718)
-
-frame_elegibles.pack(
-                     fill='both',
-                     expand=True)
-
-OfertaLabel = ctk.CTkLabel(frame_elegibles,
-                           text='Seleccione el archivo de Oferta SIES',
-                           font=('D-DIN-PRO', 16))
-OfertaLabel.pack(pady=10)
-
-Oferta_boton = ctk.CTkButton(
-    frame_elegibles,
-    text='Oferta SIES',
-    command=select_file_oferta)
-
-Oferta_boton.pack(pady=10)
-
-
-MatriculaLabel = ctk.CTkLabel(frame_elegibles,
-                              text='Seleccione el archivo de Matrícula SIES',
-                              font=('D-DIN-PRO', 16))
-MatriculaLabel.pack(pady=10)
-
-Matricula_boton = ctk.CTkButton(
-    frame_elegibles,
-    text='Matrícula SIES',
-    command=select_file_matricula)
-
-Matricula_boton.pack(pady=10)
-
-TituladosLabel = ctk.CTkLabel(frame_elegibles,
-                              text='Seleccione el archivo de Titulados SIES',
-                              font=('D-DIN-PRO', 16))
-TituladosLabel.pack(pady=10)
-
-Titulados_boton = ctk.CTkButton(
-    frame_elegibles,
-    text='Titulados SIES',
-    command=select_file_titulados)
-
-Titulados_boton.pack(pady=10)
-
-Run_Main_boton = ctk.CTkButton(
-    frame_elegibles,
-    text='Filtrar elegibles',
-    fg_color='#009a44',
-    hover_color='#005224',
-    command=Run_Main
-    )
-
-Run_Main_boton.pack(pady=10)
+        self.add("Inicio")  # add tab at the end
+        self.add("Elegibles")  # add tab at the end
+        self.add("Selección")  # add tab at the end
 
 
 # ---------------------------------------------------------------------
-# Pestaña para ejecutar programa de selección
+class FrameInicio(ctk.CTkFrame):
+    """Pestaña con descripción inicial."""
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
 
-# Función para iniciar código de selección
+# ---------------------------------------------------------------------
+class FrameElegibles(ctk.CTkFrame):
+    """Pestaña para ejecutar programa de elegibilidad."""
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-def funcion_boton():
-    caja.configure(state='normal')
-    eleccion = combobox.get()
-    seleccion.funcion_seleccion(eleccion)
-    var = 'Generando selección...'
-    caja.insert('0.0', var+'\n')
-    caja.insert('0.0', 'Se ha completado la selección de la institución:\n'
-                + eleccion + '\n')
+        self.Oferta_path = ''
+        self.Matricula_path = ''
+        self.Titulados_path = ''
 
-    caja.configure(state='disabled')
+        self.OfertaLabel = ctk.CTkLabel(
+                self,
+                text='Seleccione el archivo de Oferta SIES',
+                font=('D-DIN-PRO', 16))
+        self.OfertaLabel.pack(pady=10)
+
+        self.Oferta_boton = ctk.CTkButton(
+                self,
+                text='Oferta SIES',
+                command=self.select_file_oferta)
+
+        self.Oferta_boton.pack(pady=10)
+
+        self.MatriculaLabel = ctk.CTkLabel(
+                self,
+                text='Seleccione el archivo de Matrícula SIES',
+                font=('D-DIN-PRO', 16))
+        self.MatriculaLabel.pack(pady=10)
+
+        self.Matricula_boton = ctk.CTkButton(
+                self,
+                text='Matrícula SIES',
+                command=self.select_file_matricula)
+
+        self.Matricula_boton.pack(pady=10)
+
+        self.TituladosLabel = ctk.CTkLabel(
+                self,
+                text='Seleccione el archivo de Titulados SIES',
+                font=('D-DIN-PRO', 16))
+        self.TituladosLabel.pack(pady=10)
+
+        self.Titulados_boton = ctk.CTkButton(
+                self,
+                text='Titulados SIES',
+                command=self.select_file_titulados)
+
+        self.Titulados_boton.pack(pady=10)
+
+        self.Run_Main_boton = ctk.CTkButton(
+                self,
+                text='Filtrar elegibles',
+                fg_color='#009a44',
+                hover_color='#005224',
+                command=self.Run_Main)
+
+        self.Run_Main_boton.pack(pady=10)
+
+    def select_file_oferta(self):
+        """Función para botón de selección archivo Oferta."""
+        filetypes = (
+                ('csv (archivo separado por comas)', '.csv'),
+                ('Todos los archivos', '*.*')
+        )
+
+        global Oferta_path
+        file = fd.askopenfilename(
+                title='Abrir archivo',
+                initialdir='/',
+                filetypes=filetypes)
+        if file != '':
+            self.Oferta_boton.configure(text=file)
+        Oferta_path = file
+
+    def select_file_matricula(self):
+        """Función para botón de selección archivo Matrícula."""
+        filetypes = (
+                ('csv (archivo separado por comas)', '.csv'),
+                ('Todos los archivos', '*.*')
+        )
+
+        global Matricula_path
+        file = fd.askopenfilename(
+                title='Abrir archivo',
+                initialdir='/',
+                filetypes=filetypes)
+        if file != '':
+            self.Matricula_boton.configure(text=file)
+        Matricula_path = file
+
+    def select_file_titulados(self):
+        """Función para botón de selección archivo Titulados."""
+        filetypes = (
+                ('csv (archivo separado por comas)', '.csv'),
+                ('Todos los archivos', '*.*')
+        )
+
+        global Titulados_path
+        file = fd.askopenfilename(
+                title='Abrir archivo',
+                initialdir='/',
+                filetypes=filetypes)
+        if file != '':
+            self.Titulados_boton.configure(text=file)
+        Titulados_path = file
+
+    def Run_Main(self):
+        """Ejecuta código de elegibilidad."""
+        Main(outputfolder,
+             self.Oferta_path,
+             self.Matricula_path,
+             self.Titulados_path)
+        self.Run_Main_boton.configure(text='Elegibles listos!')
 
 
-# Función para borrar el texto en la caja de output
+# ---------------------------------------------------------------------
+class FrameSeleccion(ctk.CTkFrame):
+    """Pestaña para ejecutar programa de selección."""
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.fondo = ctk.CTkImage(light_image=Image.open("fondo2.png"),
+                                  dark_image=Image.open("fondo2.png"),
+                                  size=(274, 208))
+        self.ImageLabel = ctk.CTkLabel(master=self,
+                                       text='',
+                                       image=self.fondo)
+        self.ImageLabel.place(anchor=ctk.SW,
+                              relx=0.0,
+                              rely=1.0)
+
+        self.tituloLabel = ctk.CTkLabel(
+                self,
+                text='Le damos la bienvenida al Programa' +
+                ' para Selección de Muestra Intencionada',
+                font=('D-DIN-PRO', 24))
+        self.tituloLabel.pack(pady=10)
+
+        self.subtituloLabel = ctk.CTkLabel(
+                self,
+                text='Elija la institución para hacer la MI',
+                font=('D-DIN-PRO', 16))
+        self.subtituloLabel.pack(pady=10)
+
+        self.lista_IES = os.listdir(outputfolder)
+
+        self.lista_IES = [inst.replace('.xlsx', "") for inst in self.lista_IES]
+
+        self.combobox = ctk.CTkComboBox(self, width=500)
+        self.combobox.pack(pady=10)
+
+        CTkScrollableDropdown(self.combobox, values=self.lista_IES,
+                              justify="left", button_color="transparent",
+                              autocomplete=True)
+
+        self.boton = ctk.CTkButton(master=self,
+                                   text='Generar selección',
+                                   font=('D-DIN-PRO', 16),
+                                   command=self.funcion_boton)
+        self.boton.pack(pady=10)
+
+        self.caja = ctk.CTkTextbox(master=self, width=800,
+                                   height=250,
+                                   fg_color='light gray',
+                                   text_color='black',
+                                   font=('D-DIN-PRO', 14),
+                                   corner_radius=5,
+                                   state='disabled')
+        self.caja.pack(pady=10)
+
+        self.boton_limpiar = ctk.CTkButton(master=self,
+                                           text='Limpiar cuadro de texto',
+                                           font=('D-DIN-PRO', 16),
+                                           command=self.limpiar)
+        self.boton_limpiar.pack(pady=10)
+
+    def funcion_boton(self):
+        """Función para iniciar código de selección."""
+        self.caja.configure(state='normal')
+        eleccion = self.combobox.get()
+        seleccion.funcion_seleccion(eleccion)
+        var = 'Generando selección...'
+        self.caja.insert('0.0', var+'\n')
+        self.caja.insert('0.0',
+                         'Se ha completado la selección de la institución:\n'
+                         + eleccion + '\n')
+
+        self.caja.configure(state='disabled')
+
+    def limpiar(self):
+        """Función para borrar el texto en la caja de output."""
+        self.caja.configure(state='normal')
+        self.caja.delete('0.0', 'end')
+        self.caja.configure(state='disabled')
 
 
-def limpiar():
-    caja.configure(state='normal')
-    caja.delete('0.0', 'end')
-    caja.configure(state='disabled')
-
-
-frame_seleccion = ctk.CTkFrame(master=tabview.tab('Selección'),
-                               width=1366,
-                               height=718)
-frame_seleccion.pack(expand=True,
-                     fill='both')
-
-fondo = ctk.CTkImage(light_image=Image.open("fondo2.png"),
-                     dark_image=Image.open("fondo2.png"),
-                     size=(274, 208))
-ImageLabel = ctk.CTkLabel(master=frame_seleccion,
-                          text='',
-                          image=fondo)
-ImageLabel.place(anchor=ctk.SW,
-                 relx=0.0,
-                 rely=1.0)
-
-tituloLabel = ctk.CTkLabel(frame_seleccion,
-                           text='Le damos la bienvenida al Programa' +
-                           ' para Selección de Muestra Intencionada',
-                           font=('D-DIN-PRO', 24))
-tituloLabel.pack(pady=10)
-
-subtituloLabel = ctk.CTkLabel(frame_seleccion,
-                              text='Elija la institución para hacer la MI',
-                              font=('D-DIN-PRO', 16))
-subtituloLabel.pack(pady=10)
-
-lista_IES = os.listdir(outputfolder)
-
-lista_IES = [inst.replace('.xlsx', "") for inst in lista_IES]
-
-combobox = ctk.CTkComboBox(frame_seleccion, width=500)
-combobox.pack(pady=10)
-
-CTkScrollableDropdown(combobox, values=lista_IES, justify="left",
-                      button_color="transparent", autocomplete=True)
-
-boton = ctk.CTkButton(master=frame_seleccion,
-                      text='Generar selección',
-                      font=('D-DIN-PRO', 16),
-                      command=funcion_boton)
-boton.pack(pady=10)
-
-caja = ctk.CTkTextbox(master=frame_seleccion, width=800,
-                      height=250,
-                      fg_color='light gray',
-                      text_color='black',
-                      font=('D-DIN-PRO', 14),
-                      corner_radius=5,
-                      state='disabled')
-caja.pack(pady=10)
-
-boton_limpiar = ctk.CTkButton(master=frame_seleccion,
-                              text='Limpiar cuadro de texto',
-                              font=('D-DIN-PRO', 16),
-                              command=limpiar)
-boton_limpiar.pack(pady=10)
-
+app = App()
 app.mainloop()
